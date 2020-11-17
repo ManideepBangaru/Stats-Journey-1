3bf92a6569ba528d558a9709f27b2829f4e96357
ManideepBangaru <bmd994@gmail.com>
2020-11-18 00:01:30 +0530

    Added till Kurtosis

--- R/Chapter3.R
+++ R/Chapter3.R
@@ -15,9 +15,75 @@ median(vec) # median of the vector
 # find out the third measure
 
 # Harmonic Mean of a vector
-hm <- function(vcetor) {
+hm_ <- function(vcetor) {
   return(length(vec)/sum(1/vec))
 }
 
-hm(vec)
+hm_(vec)
 
+# geometric Mean of a vector
+gm_ <- function(vcetor) {
+  return(prod(vcetor)^(1/length(vcetor)))
+}
+gm_(vec)
+
+
+# Variation and shape
+
+# range of a vector
+range_ <- function(vcetor) {
+  return(max(vcetor)-min(vcetor))
+}
+range_(vec)
+
+# variance of a vector
+variance_ <- function(vcetor){
+    var_ <-  0
+    for (ele in 1:length(vcetor)){
+      var_ = var_ + (vcetor[ele] - mean(vcetor))^2
+    }
+    return (var_/length(vcetor)-1)
+}
+
+variance_(vec)
+
+# standard deviation of a vector
+stddev_ <- function(vcetor){
+  return (variance_(vcetor) ^ 0.5)
+}
+stddev_(vec)
+
+# Coefficient of Variation
+# It measures the scatter in the data with respect to the mean
+CoeffVar_ <- function(vcetor){
+  return (stddev_(vcetor)/mean(vcetor)) *100
+}
+CoeffVar_(vec)
+
+# Z Scores
+# a Z score of 0 indicates that the value is same as the mean
+# helps in identifying outliers, less than -3 and greater than +3 are considered to be outliers
+zscore <- function(vcetor){
+  temp_ <- c()
+  for (ele in 1:length(vcetor)){
+    temp_ <- append(temp_, (vcetor[ele]-mean(vcetor))/stddev_(vcetor))
+  }
+  return (temp_)
+}
+zscore(vec)
+
+# skewness
+# Mean < Median ----> left skewed or negative skew
+# Mean = Median ----> symmetrical distribution (zero skewness)
+# Mean > Median ----> right skewed or positive skew
+
+# Kurtosis
+# A distribution that has a sharper-rising center peak than the peak of a normal distribution has 
+# positive kurtosis, a kurtosis value that is greater than zero, and is called lepokurtic. 
+# 
+# A distribution that has a slower-rising (flatter) center peak than the peak of a normal distribution 
+# has negative kurtosis, a kurtosis value that is less than zero, and is called platykurtic. 
+# 
+# A lepokurtic distribution has a higher concentration of values near the mean of the distribution 
+# compared to a normal distribution, while a platykurtic distribution has a lower concentration 
+# compared to a normal distribution.
